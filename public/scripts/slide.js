c3217e75b707da663967ce5346e2015d0f346a62
Kohei Shingai <koheishingai@gmail.com>
2013-09-16 01:00:23 +0900

    commit

--- public/scripts/slide.js
+++ public/scripts/slide.js
@@ -728,8 +728,14 @@ SpSlidemenu.prototype.getScrollCurrentY = function() {
 
 SpSlidemenu.prototype.getTranslateX = function(x) {
     var _this = this;
+    //Device Branch
+    var userAgent = window.navigator.userAgent.toLowerCase();
     if(x != 0){
-      x = x+142;
+      if (userAgent.indexOf('crios') != -1) {
+        x = x+142;
+      }else{
+        x = x+0;
+      }
     }
     return _this.use3d ? 'translate3d(' + x + 'px, 0px, 0px)' : 'translate(' + x + 'px, 0px)';
 };
